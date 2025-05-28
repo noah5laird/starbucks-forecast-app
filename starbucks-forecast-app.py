d16f271c3b7bc055dce9b6b99adcea22e5100ec6
noah5laird <150947545+noah5laird@users.noreply.github.com>
2025-05-28 11:07:27 -0600

    Update starbucks-forecast-app.py

--- starbucks-forecast-app.py
+++ starbucks-forecast-app.py
@@ -60,7 +60,7 @@ forecasted_val = forecasted.iloc[-1]
 
 summary_text = (
     f"Based on the current forecast, Starbucks’ quarterly revenue is projected to grow steadily from its most recent "
-    f"level of ${latest:,.0f} to approximately ${forecasted_val:,.0f} in future quarters. This trend aligns with moderate "
+    f"level of ${latest} to approximately ${forecasted_val:,.0f} in future quarters. This trend aligns with moderate "
     f"CPI growth and controlled expense levels, suggesting sustainable business performance. The analysis indicates low risk "
     f"of revenue overstatement under current macroeconomic conditions. Auditors should continue to monitor these assumptions "
     f"as economic conditions evolve."
